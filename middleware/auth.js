8d612e4270dcd83d06f397c054cfd2e89342ac20
sarahg813 <goon.sarah@gmail.com>
2020-05-23 23:23:41 -0400

    edited jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const jwtSecret = process.env.JWT_SECRET;
+const jwtSecret = process.env.JWT_SECRET || "secret88";
 
 //when we want a private route
 //add this auth middleware
